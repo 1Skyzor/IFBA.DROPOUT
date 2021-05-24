152db2b380fb57c8fd43731cf681f6148d6b5c82
1Skyzor <emersonsantanadejesus@gmail.com>
2021-05-23 23:21:43 -0300

    remocao de arquivos inuteis

--- deletar_TEMP.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once ('bd/conexao.php');
-$objeto = new Conexao();
-$conexao = $objeto->Conectar();
-$consulta = "DELETE FROM usuarios WHERE usuario = 'emerson'";
-$resultado = $conexao->prepare($consulta);
-$resultado->execute();
-$data=$resultado->fetchAll(PDO::FETCH_ASSOC);
-?>
