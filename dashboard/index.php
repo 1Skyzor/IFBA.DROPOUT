46be59036c8687fd56e383b5401172a2db191d67
1Skyzor <emersonsantanadejesus@gmail.com>
2021-07-03 00:05:50 -0300

    gráficos

--- dashboard/index.php
+++ dashboard/index.php
@@ -7,75 +7,24 @@
         <meta charset="utf-8" />
         <title>Chart.js demo</title>
         <!-- import plugin script -->
-		<script	src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.min.js"></script>
-
-		
+    <style>
+      .plot {
+        display: inline-block;
+        margin: 4px;
+      }
+    </style>
     </head>
     <body>
-    <div class="container-fluid">
-			<div class="row">
-				<div class="col-md-4">
-					<div class="card mt-4">
-						<div class="card-header">Gráfico 1</div>
-						<div class="card-body">
-							<div class="chart-container pie-chart">
-								<canvas id="pie_chart"></canvas>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-4">
-					<div class="card mt-4">
-						<div class="card-header">Gráfico 2</div>
-						<div class="card-body">
-							<div class="chart-container pie-chart">
-								<canvas id="doughnut_chart"></canvas>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-4">
-					<div class="card mt-4 mb-4">
-						<div class="card-header">Nível Socioeconômico</div>
-						<div class="card-body">
-							<div class="chart-container pie-chart">
-								<canvas id="bar_chart"></canvas>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-4">
-					<div class="card mt-4 mb-4">
-						<div class="card-header">Gráfico 4</div>
-						<div class="card-body">
-							<div class="chart-container pie-chart">
-								<canvas id="chart4"></canvas>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-4">
-					<div class="card mt-4 mb-4">
-						<div class="card-header">Gráfico 5</div>
-						<div class="card-body">
-							<div class="chart-container pie-chart">
-								<canvas id="chart5"></canvas>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-4">
-					<div class="card mt-4 mb-4">
-						<div class="card-header">Gráfico 6</div>
-						<div class="card-body">
-							<div class="chart-container pie-chart">
-								<canvas id="chart6"></canvas>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+        <br><br>
+        <h4> Gráficos indicadores de evasão escolar </h4>
+        <div class="plot" id="plot1"></div>
+        <div class="plot" id="plot2"></div>
+        <div class="plot" id="plot3"></div>
+        <div class="plot" id="plot4"></div>
+        <div class="plot" id="plot5"></div>
+        <div class="plot" id="plot6"></div>
+        <div class="plot" id="plot7"></div>
+        <div class="plot" id="plot8"></div>
     </body>
  <?php
 define('PROJECT_ROOT_PATH', __DIR__);
@@ -90,7 +39,9 @@ $resultado = $conexao->prepare($consulta);
 $resultado->execute();
 $data=$resultado->fetchAll(PDO::FETCH_ASSOC);
 ?>  
-    
+<script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs"> </script>
+<script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-vis"></script>
+<script type="text/javascript" src = "js/data-graph.js"> </script>
 </div>
 <!--FIN del cont principal-->
 
